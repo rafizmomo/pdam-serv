69c10fb94f00dd6efcf9ee5005880fe017be4c3d
rafizmomo <sizansan@gmail.com>
2020-11-11 16:33:37 +0700

    Force Header Accept application/json

--- app/Http/Controllers/API/AuthController.php
+++ app/Http/Controllers/API/AuthController.php
@@ -6,7 +6,6 @@ use App\Http\Controllers\Controller;
 use App\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Validator;
-use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
 
 class AuthController extends Controller
