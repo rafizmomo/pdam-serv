bbc1e65288cb5b3448be36c39b4050f1587cd09a
rafizmomo <sizansan@gmail.com>
2020-11-12 11:14:29 +0700

    Migration Database pdam LARAVEL:API

---
+++ database/migrations/2020_11_06_042634_create_t4pelanggan_table.php
@@ -0,0 +1,76 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateT4pelangganTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('t4pelanggan', function (Blueprint $table) {
+            $table->string('idno')->unique();
+            $table->string('idpel')->primary();
+            $table->string('idjln');
+            $table->string('namapel');
+            $table->string('kelompok');
+            $table->string('ketkelompok');
+            $table->string('kdpellama');
+            $table->string('alamat');
+            $table->string('RT');
+            $table->string('RW');
+            $table->string('Desa');
+            $table->string('Kecamatan');
+            $table->string('Contact');
+            $table->string('telp');
+            $table->string('urut');
+            $table->string('Loket');
+            $table->date('last_update');
+            $table->string('user_id');
+            $table->string('idprop');
+            $table->string('rute');
+            $table->string('urutrute');
+            $table->string('mtrid');
+            $table->string('typemtr');
+            $table->date('tglinstall');
+            $table->string('jmldigit');
+            $table->string('idlama');
+            $table->date('tglbaca');
+            $table->unsignedInteger('angkamtr');
+            $table->string('lokasimtr');
+            $table->string('artikel');
+            $table->unsignedInteger('urutno');
+            $table->string('setatus');
+            $table->string('sub_id');
+            $table->string('agrtid');
+            $table->string('npa_lama');
+            $table->string('snmeter');
+            $table->string('idpbr');
+            $table->string('keterangan');
+            $table->string('merkmeter');
+            $table->string('kondisimeter');
+            $table->date('tgl_lahir');
+            $table->string('petugas');
+            $table->string('khusus');
+            $table->text('Catatan');
+            $table->string('lat');
+            $table->string('lng');
+            $table->timestamp('tanggalupd');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('t4pelanggan');
+    }
+}
---
+++ database/migrations/2020_11_06_092422_create_t4rektagih_table.php
@@ -0,0 +1,71 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateT4rektagihTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('t4rektagih', function (Blueprint $table) {
+            $table->id();
+            $table->string('idpel');
+            $table->string('idjln');
+            $table->string('kdpellama');
+            $table->string('keltarif');
+            $table->string('gol');
+            $table->string('kelompok');
+            $table->string('novocer');
+            $table->string('aktno');
+            $table->string('bulan');
+            $table->unsignedInteger('meter_sbl');
+            $table->unsignedInteger('meter_skr');
+            $table->unsignedInteger('mpakai');
+            $table->unsignedInteger('minn');
+            $table->unsignedInteger('adm');
+            $table->unsignedInteger('dmeter');
+            $table->unsignedInteger('dlain');
+            $table->unsignedInteger('denda');
+            $table->unsignedInteger('h1');
+            $table->unsignedInteger('h2');
+            $table->unsignedInteger('h3');
+            $table->unsignedInteger('h4');
+            $table->unsignedInteger('s1');
+            $table->unsignedInteger('s2');
+            $table->unsignedInteger('s3');
+            $table->unsignedInteger('s4');
+            $table->unsignedInteger('total');
+            $table->date('tanggal');
+            $table->string('loket');
+            $table->string('users');
+            $table->string('setatus');
+            $table->string('Keterangan');
+            $table->string('haspaid');
+            $table->unsignedInteger('harga');
+            $table->time('waktu');
+            $table->timestamp('tanggalupload');
+
+
+            $table->foreign('idpel')
+                ->references('idpel')
+                ->on('t4pelanggan')
+                ->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('t4rektagih');
+    }
+}
