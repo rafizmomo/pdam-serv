c6da86c69f59fec18c704ead273a078ab1ff11c4
rafizmomo <sizansan@gmail.com>
2020-11-12 10:41:03 +0700

    API, Passport, MiddlewareAdmin Done LARAVEL:API

--- app/Http/Controllers/API/AuthController.php
+++ app/Http/Controllers/API/AuthController.php
@@ -53,6 +53,7 @@ class AuthController extends Controller
             'name' => 'required|string|max:255',
             'email' => 'required|string|email|max:255|unique:users',
             'password' => 'required|string|min:6|confirmed',
+            'roles' => 'string'
         ]);
         if ($validator->fails()) {
             return response(['errors' => $validator->errors()->all()], 422);
--- routes/api.php
+++ routes/api.php
@@ -14,14 +14,24 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
-});
+//Route::middleware(['cors', 'json.response', 'auth:api'])->get('/user', function (Request $request) {
+//    return $request->user();
+//});
 
 Route::group(['middleware' => ['cors', 'json.response']], function () {
 
     // public routes
-    Route::post('/login', 'Auth\ApiAuthController@login')->name('login.api');
-    Route::post('/register', 'Auth\ApiAuthController@register')->name('register.api');
-    Route::post('/logout', 'Auth\ApiAuthController@logout')->name('logout.api');
+    Route::post('/login', 'API\AuthController@login')->name('login.api');
+    Route::post('/register', 'API\AuthController@register')->name('register.api');
+});
+
+Route::middleware('auth:api')->group(function () {
+    // our routes to be protected will go in here
+    Route::post('/logout', 'API\AuthController@logout')->name('logout.api');
+
+    Route::group(['middleware' => 'api.admin'], function () {
+        Route::get('/user', function (Request $request) {
+            return $request->user();
+        });
+    });
 });
